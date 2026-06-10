0ac910b52371389276d9eba34f3224bb185ed782
jds383 <starker_2hyphens@icloud.com>
2026-06-10 14:16:36 -0400

    Update trip.config.js

--- src/trip.config.js
+++ src/trip.config.js
@@ -7,6 +7,7 @@ export const tripConfig = {
     calendar: "fc232f0e-d153-8337-8bf9-070572d31b4b",
     activities: "37932f0ed153801c859ae950f2def6d5",
     ridePreferences: "d1b32f0e-d153-83a2-93e1-870592a25883",
+    disneyRides: "41932f0ed153827eb3e20125df01817b",
     diningCredits: null,
   },
   people: [
