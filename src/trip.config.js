61176ce8439df532ac7df4c4ffbe5007cd554b8f
jds383 <starker_2hyphens@icloud.com>
2026-06-10 12:34:35 -0400

    Create trip.config.js

---
+++ src/trip.config.js
@@ -0,0 +1,10 @@
+export const tripConfig = {
+  name: "S Family Disney Trip - August 2026",
+  tripId: "aug2026",
+  databases: {
+    calendar: "fc232f0e-d153-8337-8bf9-070572d31b4b",
+    activities: "89932f0e-d153-8203-bedc-879b3b55fe03",
+    ridePreferences: "d1b32f0e-d153-83a2-93e1-870592a25883",
+    diningCredits: null,
+  }
+}
