26e6afda5632df83e4b3bae58e8872c935d4b669
jds383 <starker_2hyphens@icloud.com>
2026-06-10 20:44:41 -0400

    Update trip.config.js

--- src/trip.config.js
+++ src/trip.config.js
@@ -3,12 +3,13 @@ export const tripConfig = {
   tripId: "aug2026",
   startDate: "2026-08-11",
   endDate: "2026-08-21",
+  family: "S",
   databases: {
-    calendar: "fc232f0e-d153-8337-8bf9-070572d31b4b",
-    activities: "37932f0ed153801c859ae950f2def6d5",
+    calendar:        "fc232f0e-d153-8337-8bf9-070572d31b4b",
+    activities:      "37932f0ed153801c859ae950f2def6d5",
     ridePreferences: "d1b32f0e-d153-83a2-93e1-870592a25883",
-    disneyRides: "41932f0ed153827eb3e20125df01817b",
-    diningCredits: "9cb32f0e-d153-83c8-b1b2-07822c7b593a",
+    disneyRides:     "41932f0ed153827eb3e20125df01817b",
+    diningCredits:   "9cb32f0e-d153-83c8-b1b2-07822c7b593a",
   },
   people: [
     { id: "J", name: "Jeffrey" },
@@ -17,5 +18,4 @@ export const tripConfig = {
     { id: "r", name: "Rebecca" },
   ],
   parks: ["mk", "ep", "hs", "ak"],
-  family: "S",
 }
